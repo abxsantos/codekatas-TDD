828fc05366e839ba27358d4ab58e46c1159908ca
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-10-11 18:32:18 -0300

    test: tests passing for supermaket name property

--- supermarket_pricing/supermarket.py
+++ supermarket_pricing/supermarket.py
@@ -2,11 +2,12 @@ import unittest
 
 
 class Supermarket(object):
-    """
-    The supermarket will contain it's name and
-    all the products registered in it.
-    """
-    pass
+    def __init__(self, name):
+        """
+        The supermarket will contain it's name and
+        all the products registered in it.
+        """
+        self.name = name
 
 
 class TestSupermarket(unittest.TestCase):
@@ -14,6 +15,10 @@ class TestSupermarket(unittest.TestCase):
     def test_supermaket_is_instantiated_with_given_name(self):
         supermarket = Supermarket('mYrket')
         self.assertEqual(supermarket.name, 'mYrket')
+
+    def test_supermaket_must_raise_error_when_no_name_is_given(self):
+        with self.assertRaises(TypeError):
+            Supermarket()
 
 
 if __name__ == '__main__':
